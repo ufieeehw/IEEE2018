d51786521c9797be4d1003c3a274db6d4b17879e
Quade <qkirby@ufl.edu>
2017-11-09 18:07:50 -0500

    ERROR:just a bad idea in general

--- ieee_ws/build/Bog/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/orangehoopla/Desktop/IEEE2018/ieee_ws/src/Bog
-# Build directory: /home/orangehoopla/Desktop/IEEE2018/ieee_ws/build/Bog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
