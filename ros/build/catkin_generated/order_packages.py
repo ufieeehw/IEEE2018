196d6b7c066de9dba22b187b52c6b61d81834c2a
Quade <qkirby@ufl.edu>
2017-11-09 18:12:53 -0500

    FIX:removing issues

--- ros/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/orangehoopla/Desktop/IEEE2018/ros/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/orangehoopla/Desktop/rostests/catkin_ws/devel;/home/orangehoopla/mil_ws/devel;/opt/ros/kinetic".split(';') if "/home/orangehoopla/Desktop/rostests/catkin_ws/devel;/home/orangehoopla/mil_ws/devel;/opt/ros/kinetic" != "" else []
