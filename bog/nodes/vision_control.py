5f1c21e4c7c0c5e16e671d6e638bc006927344d3
Kaden King <kingk45@ufl.edu>
2018-03-25 22:30:40 -0400

    VISION: added callback methods for vision and direction topics.

--- bog/nodes/vision_control.py
+++ bog/nodes/vision_control.py
@@ -3,20 +3,17 @@ import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float32
 
+def distance_callback(data):
+    rospy.loginfo("Distance: %s", data.data)
+
+
+def direction_callback(data):
+    rospy.loginfo("Direction: %s", data.data)
 
 def listen():
-    direct = rospy.Publisher('direction', String, queue_size=10) #handles the publishing of the direction of the button
-    dist = rospy.Publisher('distance', Float32, queue_size=10) #handles the publishing of the approximate distance from the camera to the button
-    rospy.init_node('seeker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-
-
-    dist.publish(distance_number)
-    direct.publish(direction)
-    rospy.loginfo(direction)
-    rospy.loginfo(distance_number)
-    rate.sleep()
-
+    rospy.init_node('vision_subscriber', anonymous=True)
+    rospy.Subscriber('direction', String, direction_callback)
+    rospy.Subcriber('distance', Float32, distance_callback)
 
 
 if __name__ == '__main__':
