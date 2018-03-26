25daade9c16920e722a2e6b79153652e87d42471
Kaden King <kingk45@ufl.edu>
2018-03-25 22:49:42 -0400

    VISION: added contants that we can change before each launch for easy changes during competition

--- bog/nodes/vision_control.py
+++ bog/nodes/vision_control.py
@@ -3,17 +3,41 @@ import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float32
 
-def distance_callback(data):
-    rospy.loginfo("Distance: %s", data.data)
+#constants
+DISTANCE_RATIO = 0.25 #this constant will be used to determine how much to move the robot left or right. The farther away we are, the less we want to move left or right. 
+GOAL_DISTANCE_LOW = 9 # the lower bound of the goal distance from the button
+GOAL_DISTANCE_HIGH = 11 # the upper bound of the goal distance from the button
 
+
+#state variables
+current_direction = ""
+current_distance = -1 
+updated = False
+
+#move method will move the robot left, right, or forward depending on the distance and direction passed to it.
+#we will move toward the button until certain parameters are met
+def move(direction, distance): 
+
+
+
+def distance_callback(data):
+    current_distance = data.data # sets current distance to the updated value of the distance topic
+    rospy.loginfo("Distance: %s", current_distance) # write the information to the console
+    updated = True
 
 def direction_callback(data):
-    rospy.loginfo("Direction: %s", data.data)
+    current_direction = data.data # sets the current direction variable to the updated value of the topic
+    rospy.loginfo("Direction: %s", current_direction) # write the information to the console
+    updated = True
 
 def listen():
     rospy.init_node('vision_subscriber', anonymous=True)
     rospy.Subscriber('direction', String, direction_callback)
     rospy.Subscriber('distance', Float32, distance_callback)
+
+    if updated: #only call the move function if we are working with new information 
+        move(direction,distance)
+        updated = False # changed updated to false so that we do not move again based on old information
 
 
     rospy.spin()
