974fb2367dc87f873382aee7ab8611af2058ac61
Kaden King <kingk45@ufl.edu>
2018-04-03 14:52:46 -0400

    IMU: created rotateTo function

--- bog/nodes/imu.py
+++ bog/nodes/imu.py
@@ -1,10 +1,10 @@
 
 
-DELAY_CONSTANT = 1 #
+DELAY_CONSTANT = 1 # stops the turning before the goal by a specified amount to account for delays
 
-class Current:
+class IMU:
     def __init__(self):
-        self._angle = 0
+        self._angle = 180
 
     def setAngle(self, angle):
         self._angle = angle
@@ -12,17 +12,42 @@ class Current:
     def angle(self):
         return self._angle
 
-    def rotate(angle):
-        #move motors to rotate 
+    def rotateTo(self,goal):
+        rotation = self._compare(goal)
+        
+        if(rotation < 0): #rotating clockwise
+            rotation = abs(rotation)
+            print("rotating clockwise " + str(rotation) + " degrees\n")
+        elif(rotation > 0): #rotating counter-clockwise
+            print("rotating counter-clockwise " + str(rotation) + " degrees")
 
-current = Current()
-goal = Goal()
-
-def compare(current,goal):
 
 
-    difference = goal._angle - current._angle
-    return difference
 
-def loop():
+    def _compare(self,goal):
+        difference = goal - self._angle; 
+        if(difference > 180):
+            difference = 180 - difference
+        elif(difference < -180):
+            difference = -180 - difference
 
+        self._angle = goal
+
+        return difference
+
+
+
+imu = IMU()
+
+
+
+
+
+def test():
+    goal = 0;
+    imu.rotateTo(goal)
+
+
+
+
+test()
