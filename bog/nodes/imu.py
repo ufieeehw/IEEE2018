c0097f023257e921de25422e42dfc7752adbdf50
Kaden King <kingk45@ufl.edu>
2018-04-07 16:53:48 -0400

    IMU: started working on setting motor speeds

--- bog/nodes/imu.py
+++ bog/nodes/imu.py
@@ -1,6 +1,47 @@
-
+#!/usr/bin/env python
+import rospy
+import time
+from std_msgs.msg import String
+from std_msgs.msg import Float32
+from bog.msg import SetWheelSpeeds
 
 DELAY_CONSTANT = 1 # stops the turning before the goal by a specified amount to account for delays
+WHEEL_SPEED = 50
+
+wheels = SetWheelSpeeds()
+
+
+def turnCCW():
+    pub = rospy.Publisher('Set_Motors', SetWheelSpeeds, queue_size=10)
+    wheels.wheel1 = -1 * WHEEL_SPEED
+    wheels.wheel2 = WHEEL_SPEED
+    wheels.wheel3 = WHEEL_SPEED
+    wheels.wheel4 = -1 * WHEEL_SPEED
+    pub.publish(wheels)
+    time.sleep(0.5)
+    wheels.wheel1 = 0
+    wheels.wheel2 = 0
+    wheels.wheel3 = 0
+    wheels.wheel4 = 0
+    pub.publish(wheels)
+    time.sleep(0.5)
+
+def turnCW():
+    pub = rospy.Publisher('Set_Motors', SetWheelSpeeds, queue_size=10)
+    wheels.wheel1 = 1 * WHEEL_SPEED
+    wheels.wheel2 = -1 * WHEEL_SPEED
+    wheels.wheel3 = -1 * WHEEL_SPEED
+    wheels.wheel4 =  1 * WHEEL_SPEED
+    pub.publish(wheels)
+    time.sleep(0.5)
+    wheels.wheel1 = 0
+    wheels.wheel2 = 0
+    wheels.wheel3 = 0
+    wheels.wheel4 = 0
+    pub.publish(wheels)
+    time.sleep(0.5)    
+
+
 
 class IMU:
     def __init__(self):
@@ -18,8 +59,11 @@ class IMU:
         if(rotation < 0): #rotating clockwise
             rotation = abs(rotation)
             print("rotating clockwise " + str(rotation) + " degrees\n")
+            turnCW()
+            turn
         elif(rotation > 0): #rotating counter-clockwise
             print("rotating counter-clockwise " + str(rotation) + " degrees")
+            turnCCW()
 
 
 
@@ -43,7 +87,9 @@ imu = IMU()
 
 
 
-def test():
+def listen():
+    rospy.init_node('vision_subscriber', anonymous=True)
+
     goal = 0;
     imu.rotateTo(goal)
 
@@ -51,3 +97,7 @@ def test():
 
 
 test()
+
+
+if __name__ == '__main__':
+    listen()
