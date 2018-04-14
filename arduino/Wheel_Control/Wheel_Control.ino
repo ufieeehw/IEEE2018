5d0a879e184ca4a0ab7071acfdf4842b3187d88d
OrangeHoopla <quadekirby@gmail.com>
2018-04-14 12:17:20 -0400

    SERIAL: changed naming conventions slightly

--- arduino/Wheel_Control/Wheel_Control.ino
+++ arduino/Wheel_Control/Wheel_Control.ino
@@ -1,113 +1,183 @@
+//#if (ARDUINO >= 100)
+// #include <Arduino.h>
+//#else
+ //#include <WProgram.h>
+//#endif
 
-Skip to content
-This repository
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+#include <bog/SetWheelSpeeds.h>
 
-    Pull requests
-    Issues
-    Marketplace
-    Explore
+#include "DualVNH5019MotorShield.h"
 
-    @OrangeHoopla
+ros::NodeHandle  nh;
+// range is -540 to 540
+int count = 0;
 
-11
-1
+int pulse_duration = 0;
+int toggle1 = 0;
 
-    3
+int count2 = 0;
+int toggle2 = 0;
 
-ufieeehw/IEEE2018
-Code
-Issues 2
-Pull requests 0
-Projects 0
-Wiki
-Insights
-Settings
-IEEE2018/arduino/IMU
-8850341 8 minutes ago
-@grimmonkey52 grimmonkey52 Update IMU
-69 lines (56 sloc) 1.59 KB
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
-#include <math.h>
+int count3 = 0;
+int toggle3= 0;
 
- 
-/* This driver reads raw data from the BNO055
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3V DC
-   Connect GROUND to common ground
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-*/
+int count4 = 0;
+int toggle4 = 0;
 
-/* Set the delay between fresh samples. Wait for serial input to define sample rate. */
+int MOTOR1 = 32;
+int MOTOR2 = 30;
+int MOTOR3 = 28;
+int MOTOR4 = 26;
+
+static unsigned long lastRefreshTime = 0;
 
 
-Adafruit_BNO055 bno = Adafruit_BNO055();
+DualVNH5019MotorShield md;
 
-/*
-void setup(void){
-   while(!Serial.available()){
-      #define BNO055_SAMPLERATE_DELAY_MS (Serial.read())
-   }
-  //remove the above serial read after testing and timing diagram is established.
-  Serial.begin(9600);
+void M1( bog::SetWheelSpeeds cmd_msg) {
+  md.setM1Speed(cmd_msg.wheel3);
+  md.setM2Speed(-cmd_msg.wheel4);
+  md.setM3Speed(-cmd_msg.wheel1);
+  md.setM4Speed(cmd_msg.wheel2);//set servo angle, should be from 0-180  
+  //stopIfFault();
+  
+    //toggle led  
+}
 
-  /* Initialise the sensor */
-  if(!bno.begin())
+void M2( bog::SetWheelSpeeds cmd_msg) {
+  md.setM1Speed(cmd_msg.wheel3);
+  md.setM2Speed(-cmd_msg.wheel4);
+  md.setM3Speed(-cmd_msg.wheel1);
+  md.setM4Speed(cmd_msg.wheel2);//set servo angle, should be from 0-180  
+  //stopIfFault();
+  
+    //toggle led  
+}
+
+
+void stopIfFault()
+{
+  if (md.getM1Fault())
   {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.println("ConnectionFail");
+    Serial.println("M1 fault");
     while(1);
+  }
+  if (md.getM2Fault())
+  {
+    Serial.println("M2 fault");
+    while(1);
+  }
+  if (md.getM3Fault())
+  {
+    Serial.println("M3 fault");
+    while(1);
+  }
+  if (md.getM4Fault())
+  {
+    Serial.println("M4 fault");
+    while(1);
+  }
+}
+
+typedef  void (*SetWheelSpeeds)  ( const bog::SetWheelSpeeds&);
+
+ros::Subscriber< bog::SetWheelSpeeds> sub("Set_Motors",(SetWheelSpeeds)M1); 
+
+bog::SetWheelSpeeds str_msg;
+ros::Publisher chatter("Wheel_feedback", &str_msg);
+
+//ros::Subscriber<std_msgs::UInt16> sub("servo", M1);
+void setup(){
+  
+  Serial.begin(57600);
+  nh.initNode();
+  nh.subscribe(sub);
+  nh.advertise(chatter);
+
+
+  pinMode(30, INPUT_PULLUP);
+
+
+  
+//  nh.subscribe(sub2);
+  
+  md.init();
+}
+
+
+void loop(){
+  nh.spinOnce();
+  str_msg.wheel1 = count;
+  str_msg.wheel2 = count2;
+  str_msg.wheel3 = count3;
+  str_msg.wheel4 = count4;
+  //chatter.publish( &str_msg );
+
+
+  if(digitalRead(MOTOR1) == 0)
+  {
+    toggle1 = 0;
+  }
+  else if(digitalRead(MOTOR1) == 1 && toggle1 == 0)
+
+  {
+    toggle1 = 1;
+    count++;
   }
 
 
-  bno.setExtCrystalUse(false);
+  
 
-  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
+  if(digitalRead(MOTOR2) == 0)
+  {
+    toggle2 = 0;
+  }
+  else if(digitalRead(MOTOR2) == 1 && toggle2 == 0)
+  {
+    toggle2 = 1;
+    count2++;
+  }
+  
 
+  if(digitalRead(MOTOR3) == 0)
+  {
+    toggle3 = 0;
+  }
+  else if(digitalRead(MOTOR3) == 1 && toggle3 == 0)
+  {
+    toggle3 = 1;
+    count3++;
+  } 
+
+
+  if(digitalRead(MOTOR4) == 0)
+  {
+    toggle4 = 0;
+  }
+  else if(digitalRead(MOTOR4) == 1 && toggle4 == 0)
+  {
+    toggle4 = 1;
+    count4++;
+  } 
+
+
+
+ 
+
+  if(millis() - lastRefreshTime >= 50)
+  {
+    lastRefreshTime += 50;
+    chatter.publish( &str_msg );
+    count = 0;
+    count2 = 0;
+    count3 = 0;
+    count4 = 0;
+    //chatter.publish( &str_msg );
+
+  }
+
+  
+  
 }
-void loop(){
-  Serial.println(IMU_data());
-}
-*/
-
-String IMU_data()
-{
- String data;
- double angle;
-  // Possible vector values can be:
-  // - VECTOR_ACCELEROMETER - m/s^2
-  // - VECTOR_MAGNETOMETER  - uT
-  // - VECTOR_GYROSCOPE     - rad/s
-  // - VECTOR_EULER         - degrees
-  // - VECTOR_LINEARACCEL   - m/s^2
-  // - VECTOR_GRAVITY       - m/s^2
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  angle = atan2(euler.y(),euler.x())
-  /* Display the floating point data */
-  data = "angle[ " + String(angle) + "] z[ " + String(euler.z() + "]");
-  return data;
-}
-
-    © 2018 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Help
-
-    Contact GitHub
-    API
-    Training
-    Shop
-    Blog
-    About
-
-Press h to open a hovercard with more details.
-
