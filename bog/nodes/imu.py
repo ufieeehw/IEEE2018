0aecd4244cd45692f0d84ef744f326f246eec740
Kaden King <kingk45@ufl.edu>
2018-04-15 14:22:09 -0400

    IMU: accurate to 2 degrees

--- bog/nodes/imu.py
+++ bog/nodes/imu.py
@@ -7,63 +7,92 @@ from geometry_msgs.msg import Vector3
 from bog.msg import SetWheelSpeeds
 
 DELAY_CONSTANT = 10 # stops the turning before the goal by a specified amount to account for delays
-WHEEL_SPEED = 50
+DELAY_CONSTANT_FINE = 4
+WHEEL_SPEED = 90
+WHEEL_SPEED_FINE = 60
 TIMEOUT = 0.5
 
 wheels = SetWheelSpeeds()
 
-def angleCorrect(angle):
-    newAngle = angle
-    if angle <= 0:
-        angle = 360 + angle
-    elif angle >= 360:
-        newAngle = angle - 360
-
-    return newAngle
 
 
 
 
-def turnCCW(rotation,goal):
+def turnCCW(distance,goal):
     global IMU
     initialAngle = IMU.angle()
-    pub = rospy.Publisher('IMU_Motors', SetWheelSpeeds, queue_size=10)
+
+    pub = rospy.Publisher('IMU_Motors', SetWheelSpeeds, queue_size=1)
     wheels.wheel1 = -1 * WHEEL_SPEED
     wheels.wheel2 = WHEEL_SPEED
     wheels.wheel3 = WHEEL_SPEED
     wheels.wheel4 = -1 * WHEEL_SPEED
-    pub.publish(wheels)
 
-    difference = angleCorrect(abs(initialAngle - IMU.angle()))
-    while(difference < rotation - DELAY_CONSTANT):
-        difference = angleCorrect(abs(initialAngle - IMU.angle()))
+
+    garb, currentDist = angleDirection(goal, IMU.angle())
+    while(currentDist > DELAY_CONSTANT):
+        garb, currentDist = angleDirection(goal, IMU.angle())
         rospy.loginfo("rotating CCW. currently at angle %s", IMU.angle())
-        rospy.loginfo("rotation: %s", rotation)
-        rospy.loginfo("difference: %s", difference)
+        rospy.loginfo("difference: %s", currentDist)
+        pub.publish(wheels)
+        time.sleep(0.05)
 
     stop()
 
 
-def turnCW(rotation,goal):
+def turnCW(distance,goal):
     global IMU
     initialAngle = IMU.angle()
-    pub = rospy.Publisher('IMU_Motors', SetWheelSpeeds, queue_size=10)
+
+
+    pub = rospy.Publisher('IMU_Motors', SetWheelSpeeds, queue_size=1)
     wheels.wheel1 = 1 * WHEEL_SPEED
     wheels.wheel2 = -1 * WHEEL_SPEED
     wheels.wheel3 = -1 * WHEEL_SPEED
     wheels.wheel4 =  1 * WHEEL_SPEED
-    pub.publish(wheels)
-    difference = angleCorrect(abs(initialAngle - IMU.angle()))
-    while(difference < rotation - DELAY_CONSTANT):
-        difference = angleCorrect(abs(initialAngle - IMU.angle()))
+    
+
+    garb, currentDist = angleDirection(goal, IMU.angle())
+    while(currentDist > DELAY_CONSTANT):
+        garb, currentDist = angleDirection(goal, IMU.angle())
         rospy.loginfo("rotating CW. currently at angle %s", IMU.angle())
-        rospy.loginfo("rotation: %s", rotation)
-        rospy.loginfo("difference: %s", difference)
+        rospy.loginfo("difference: %s", currentDist)
+        pub.publish(wheels)
+        time.sleep(0.05)
+
+    while (currentDist > DELAY_CONSTANT_FINE):
+        direction, currentDist = angleDirection(IMU.angle(),goal)
+        IMU.rotateToFine(direction,currentDist,goal)
 
     stop()
 
+
+def turnCWFine(distance,goal):
+
+
+    pub = rospy.Publisher('IMU_Motors', SetWheelSpeeds, queue_size=1)
+    wheels.wheel1 = 1 * WHEEL_SPEED_FINE
+    wheels.wheel2 = -1 * WHEEL_SPEED_FINE
+    wheels.wheel3 = -1 * WHEEL_SPEED_FINE
+    wheels.wheel4 =  1 * WHEEL_SPEED_FINE
+    pub.publish(wheels)
+    time.sleep(0.3)
+    stop()    
+    time.sleep(1)
+
+def turnCCWFine(distance,goal):
+    pub = rospy.Publisher('IMU_Motors', SetWheelSpeeds, queue_size=1)
+    wheels.wheel1 = -1 * WHEEL_SPEED_FINE
+    wheels.wheel2 = 1 * WHEEL_SPEED_FINE
+    wheels.wheel3 = 1 * WHEEL_SPEED_FINE
+    wheels.wheel4 = -1 * WHEEL_SPEED_FINE
+    pub.publish(wheels)
+    time.sleep(0.3)
+    stop()    
+    time.sleep(1)
+
 def stop():
-    pub = rospy.Publisher('IMU_Motors', SetWheelSpeeds, queue_size=10)
+    pub = rospy.Publisher('IMU_Motors', SetWheelSpeeds, queue_size=1)
     wheels.wheel1 = 0
     wheels.wheel2 = 0
     wheels.wheel3 = 0
@@ -89,16 +118,20 @@ class IMU:
     def getZero(self):
         return self._zero
 
-    def rotateTo(self,goal):    
-        rotation = self.compare(goal)
-        if(rotation > 0): #rotating clockwise
-            rotation = abs(rotation)
-            print("rotating clockwise " + str(rotation) + " degrees\n")
-            turnCW(angleCorrect(rotation),goal)
-        elif(rotation < 0): #rotating counter-clockwise
-            rotation = abs(rotation)
-            print("rotating counter-clockwise " + str(rotation) + " degrees")
-            turnCCW(angleCorrect(rotation),goal)
+    def rotateTo(self,direction,distance,goal):
+        if direction == "CW":
+            turnCW(distance,goal)
+        elif direction == "CCW":
+            turnCCW(distance,goal)
+
+    def rotateToFine(self,direction,distance,goal):
+        if direction == "CW":
+            turnCWFine(distance,goal)
+        elif direction == "CCW":
+            turnCCWFine(distance,goal)
+
+
+        
 
     def compare(self,goal):
         difference = abs(goal - self._current_angle); 
@@ -112,7 +145,7 @@ class IMU:
 
 IMU = IMU()
 
-def angleDirection(currentAngle,nextAngle): # 0 -> CW, 1 -> CCW
+def angleDirection(currentAngle,nextAngle): 
     difference = nextAngle - currentAngle
     if difference <= 0:
         if difference < -180:
@@ -134,18 +167,18 @@ def rotation_callback(data):
     #rospy.loginfo("IMU: %s", data.x)
 
 def goal_callback(data):
-    #IMU.rotateTo(data.data)
-    direction, distance = angleDirection(0,data.data)
-
+    
+    direction, distance = angleDirection(IMU.angle(),data.data)
     rospy.loginfo("Goal: %s", data.data)
     rospy.loginfo("Direction: %s", direction)
     rospy.loginfo("Distance: %s", distance)
+    IMU.rotateTo(direction,distance,data.data)
 
 
 
 def listen():
     rospy.init_node('vision_subscriber', anonymous=True)
-    #rospy.Subscriber("Port_0/Imu_publisher", Vector3, rotation_callback)
+    rospy.Subscriber("Port_0/Imu_publisher", Vector3, rotation_callback)
     rospy.Subscriber("Goal_Angle", Float32, goal_callback)
 
     rospy.spin()
